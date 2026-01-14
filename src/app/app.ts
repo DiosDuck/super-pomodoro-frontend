b0a38f48a1a9de9d9f793fd7468b3f93bf7bc3dc
DiosDuck <tapucovidiu@gmail.com>
2026-01-14 20:42:13 +0200

    revert svg changes (todo on later update)

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,9 +3,6 @@ import { Index as Navbar } from "./shared/components/navbar/index/index";
 import { RouterOutlet } from '@angular/router';
 import { Index as Toast } from './shared/components/toast/index';
 import { LoaderService } from './shared/services/loader';
-import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
-import { firstValueFrom } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -14,18 +11,6 @@ import { HttpClient } from '@angular/common/http';
   styleUrl: './app.scss'
 
 })
-export class App implements OnInit {
-  sprite!: SafeHtml;
-
+export class App {
   loaderService = inject(LoaderService);
-  private _http = inject(HttpClient);
-  private _sanitizer = inject(DomSanitizer);
-
-  async ngOnInit(): Promise<void> {
-    const rawSvg = await firstValueFrom(
-      this._http.get('assets/icons/sprite.svg', { responseType: 'text' })
-    );
-
-    this.sprite = this._sanitizer.bypassSecurityTrustHtml(rawSvg);
-  }
 }
