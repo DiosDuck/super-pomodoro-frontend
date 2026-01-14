bc612e669d5091d244daae68b208a4ef3425eb11
DiosDuck <tapucovidiu@gmail.com>
2026-01-14 12:53:34 +0200

    load less times the svg file

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,11 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { Index as Navbar } from "./shared/components/navbar/index/index";
 import { RouterOutlet } from '@angular/router';
 import { Index as Toast } from './shared/components/toast/index';
 import { LoaderService } from './shared/services/loader';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { firstValueFrom } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +14,18 @@ import { LoaderService } from './shared/services/loader';
   styleUrl: './app.scss'
 
 })
-export class App {
+export class App implements OnInit {
+  sprite!: SafeHtml;
+
   loaderService = inject(LoaderService);
+  private _http = inject(HttpClient);
+  private _sanitizer = inject(DomSanitizer);
+
+  async ngOnInit(): Promise<void> {
+    const rawSvg = await firstValueFrom(
+      this._http.get('assets/icons/sprite.svg', { responseType: 'text' })
+    );
+
+    this.sprite = this._sanitizer.bypassSecurityTrustHtml(rawSvg);
+  }
 }
