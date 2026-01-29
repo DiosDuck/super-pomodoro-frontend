432e61d2dd57a0acf7f2d7654d050cd4536ccdc2
DiosDuck <tapucovidiu@gmail.com>
2026-01-29 11:10:29 +0200

    remove user service from auth

--- src/app/layout/navbar/auth/auth.ts
+++ src/app/layout/navbar/auth/auth.ts
@@ -2,7 +2,7 @@ import { Component, inject, OnInit, output, signal } from '@angular/core';
 import { Router, RouterLink } from "@angular/router";
 import { LastRouteService } from '../../../shared/utils/last-route.service';
 import { navId } from '../navbar.model';
-import { AuthService, NullableUser, UserService } from '../../../auth/auth.service';
+import { AuthService, NullableUser } from '../../../auth/auth.service';
 
 @Component({
   selector: 'app-nav-auth',
@@ -11,7 +11,6 @@ import { AuthService, NullableUser, UserService } from '../../../auth/auth.servi
   styleUrl: './auth.scss',
 })
 export class Auth implements OnInit {
-  userService = inject(UserService);
   authService = inject(AuthService)
   router = inject(Router);
   lastRouteService = inject(LastRouteService);
@@ -21,7 +20,7 @@ export class Auth implements OnInit {
   onSelect = output<navId>();
 
   ngOnInit(): void {
-    this.userService.user.subscribe(user => this.user.set(user));
+    this.authService.getObservableUser().subscribe(user => this.user.set(user));
   }
 
   onLogout(): void
